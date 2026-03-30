8b1bd5d61c9b2d681cf1c68dd4b93ca9b43cc0c4
EzequielSecchi <ezequielsecchi2@gmail.com>
2026-03-30 11:40:11 -0300

    Sacamos comentarios

--- code.py
+++ code.py
@@ -1,8 +1,4 @@
-#Programa que lea el nombre y apellido de una persona en un archivo
-#de texto llamado "nombres.txt", crear un mail con la siguiente
-#estructura: [3 primeras letras del nombre][apellido]@gmail.com, crear
-#una contraseña aleatoria de 8 digitos, y guardar mail y contraseña
-#en otro archivo de tipo "csv"
+
 
 from funciones.funciones import (
     leer_nombres,
